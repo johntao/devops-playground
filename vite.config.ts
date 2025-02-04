731771bf9be293588d31fe4e1d86af0e79e7213c
John Tao <80338582+johntao@users.noreply.github.com>
2025-02-04 20:30:08 +0800

    feat: use github packages
    refactor: update vite.config.ts to alias 'dummy1' to '@johntao/dummy1'
    
    chore: allow deployment from 'gh-package' branch in workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ export default defineConfig({
   base: "/devops-playground/",
   // build: {
   //   rollupOptions: {
-  //     external: ['@johntao/dummy1']
+  //     external: ['dummy1']
   //   },
   // },
-  // resolve: {
-  //   alias: {
-  //     '@johntao/dummy1': '/node_modules/@johntao/dummy1',
-  //   },
-  // },
+  resolve: {
+    alias: {
+      'dummy1': '@johntao/dummy1',
+    },
+  },
 });
