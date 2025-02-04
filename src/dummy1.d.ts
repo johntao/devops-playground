7b7ede6c480761158613b7d531e0e945d3aa7108
John Tao <80338582+johntao@users.noreply.github.com>
2025-02-04 14:11:10 +0800

    refactor: change dummy1 module import to use simplified package name

--- src/dummy1.d.ts
+++ src/dummy1.d.ts
@@ -1,5 +1,5 @@
    // dummy1.d.ts
-   declare module '@johntao/dummy1' {
+   declare module 'dummy1' {
     export function foo(): string;
     // Add more function/type definitions as needed
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/devops-playground/",
-  build: {
-    rollupOptions: {
-      external: ['@johntao/dummy1']
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['@johntao/dummy1']
+  //   },
+  // },
+  // resolve: {
+  //   alias: {
+  //     '@johntao/dummy1': '/node_modules/@johntao/dummy1',
+  //   },
+  // },
 });
